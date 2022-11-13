04b11dd4cbb085ec9254b4c3f78248a4c76f7764
Nithu.kb <115065205+nithikb123@users.noreply.github.com>
2022-11-13 20:18:07 +0530

    Add files via upload

---
+++ Experionproject/Experionproject/Employees.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experionproject
+{
+    internal class Employees
+    {
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        private int _salary;
+
+        public int Salary
+        {
+            get { return _salary; }
+        }
+
+        private string _designation;
+
+        public string Designation
+        {
+            get { return _designation; }
+            set {
+                if (value== "Trainee") 
+                {
+                    _salary = 5000;
+                }
+                if (value == "Manager") 
+                {
+                    _salary = 200000;
+                }
+                if(value == "Senior employee")
+                {
+                    _salary = 130000;
+                }
+                _designation = value;   
+            }
+        }
+
+
+    }
+}
---
+++ Experionproject/Experionproject/Program.cs
@@ -0,0 +1,25 @@
+﻿using Experionproject;
+
+var p1 = new Employees()
+{
+    Name = "Anoop",
+    Department = "A",
+    Designation = "Trainee",
+   
+};
+var p2 = new Employees()
+{
+    Name = "Namitha",
+    Department = "B",
+    Designation = "Manager",
+
+};
+Console.WriteLine(p1.Name);
+Console.WriteLine(p1.Department);
+Console.WriteLine(p1.Designation);
+Console.WriteLine(p1.Salary);
+
+Console.WriteLine(p2.Name);
+Console.WriteLine(p2.Department);
+Console.WriteLine(p2.Designation);
+Console.WriteLine(p2.Salary);
